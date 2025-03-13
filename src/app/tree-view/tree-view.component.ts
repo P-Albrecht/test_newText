7205b64b79d03d8744b96035066ec3905f0eb444
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2025-03-13 17:24:17 +0100

    Update tree-view.component.ts

--- src/app/tree-view/tree-view.component.ts
+++ src/app/tree-view/tree-view.component.ts
@@ -4,11 +4,19 @@ import {MatTreeNestedDataSource, MatTreeModule} from '@angular/material/tree';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { EventEmitter } from '@angular/core';
-
+// test 
+// test 
+// test 
+// test 
 
 interface TreeNode {
   name: string;
   children?: TreeNode[];
 }
 
+
+// test 
+// test 
+// test 
+// test 
 
